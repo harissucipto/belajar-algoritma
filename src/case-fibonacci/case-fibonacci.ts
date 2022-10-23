bf82611b02c0623335611ec01af0e2f3769c8f43
fadli <fadli.muharram@prodiginow.com>
2022-10-23 09:41:29 +0700

    feat: case fibonacci

---
+++ src/case-fibonacci/case-fibonacci.ts
@@ -0,0 +1,19 @@
+// Bilangan ke-10 pada suatu barisan Fibonacci yaitu
+
+export const countFibonacciSequence = (n: number) => {
+    let n0 = 0, n1 = 1, nextValue;
+    let value;
+
+    for(let x = 0; x <= n; x++){
+        if(n === x){
+            value = n0   
+        }
+        nextValue = n0 + n1;
+        n0 = n1;
+        n1 = nextValue;
+      
+    } 
+
+    return value
+}
+
